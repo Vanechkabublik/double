7d211b97240c0ee6b5b964158428c31412b228e3
Vanechkabublik <leonarov@vk.com>
2024-11-26 23:39:13 +0300

    Penis

--- store/store.ts
+++ store/store.ts
@@ -5,7 +5,7 @@ export const useGameState = () => {
   // Загружаем баланс из кук, если он существует
   const storedBalance = Cookies.get('balance'); // Получаем значение из куки
   const state = reactive({
-    balance: storedBalance ? parseFloat(storedBalance) : 1000, // Если куки нет, устанавливаем дефолтный баланс
+    balance: storedBalance ? parseFloat(storedBalance) : 0, // Если куки нет, устанавливаем дефолтный баланс
     updateBalance(amount: number) {
       state.balance += amount;
       Cookies.set('balance', state.balance.toString(), { expires: 7 }); // Сохраняем новый баланс в куки с истечением через 7 дней
